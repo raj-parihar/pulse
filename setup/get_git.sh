817b1218874622c6672fe310a66d625021ea8ae5
Raj Parihar <prince.parihar@gmail.com>
2018-11-24 02:27:44 +0000

    checking scripts

---
+++ setup/get_git.sh
@@ -0,0 +1,6 @@
+sudo git init
+git config --global user.name "Raj Parihar"
+git config --global user.email prince.parihar@gmail.com
+sudo git remote add origin https://github.com/raj-parihar/pulse
+sudo rm -rf index.html 
+sudo git pull origin master
---
+++ setup/push_git.sh
@@ -0,0 +1,3 @@
+sudo git add *
+sudo git commit -m "converted to php"
+sudo git push --force -u origin master
