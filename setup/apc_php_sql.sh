f772490a308fe1bc2559be0f5c686c4d7876866c
Raj Parihar <prince.parihar@gmail.com>
2018-11-25 07:36:55 +0000

    php connected

--- setup/apc_php_sql.sh
+++ setup/apc_php_sql.sh
@@ -1,3 +1,7 @@
 sudo apt-get update && sudo apt-get install apache2 -y
 sudo apt-get install apache2 php libapache2-mod-php
 sudo apt-get install mysql-server php-mysql php-pear
+sudo apt install php-mysqli
+sudo a2enmod proxy_fcgi setenvif
+sudo a2enconf php7.0-fpm
+sudo service apache2 restart
